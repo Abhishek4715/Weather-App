6f6d419be112f7e386095510d20c86016ea9caf9
Abhishek <abhisheksahani4715@gmail.com>
2026-07-19 20:19:51 +0530

    Created input to search places

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-// import { useState } from 'react'
-// import './App.css'
+// import { useState } from 'react';
+// import './App.css';
+import { Body } from "./components/Body.tsx";
 
 function App() {
 
   return (
-    <div>Hello</div>
+    <div className="flex flex-col justify-center items-center gap-2">
+      <Body />
+    </div>
   )
 }
 
---
+++ src/components/Body.tsx
@@ -0,0 +1,11 @@
+export function Body() {
+    return (
+        <>
+            <div>
+                <input type="search" placeholder="Search Places"
+                    className="absolute top-0 right-0 w-7/20 px-4 py-2 border-2 border-gray-400 outline-none focus:shadow-md"
+                />
+            </div>
+        </>
+    )
+}
