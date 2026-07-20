82b1282ed8eb58d6a752bd4785552b0eb280e97e
Abhishek <abhisheksahani4715@gmail.com>
2026-07-20 23:11:17 +0530

    Add Basic weather information

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,7 +3,20 @@ import { useState, useEffect } from "react";
 export function Body() {
     const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
     console.log("API: " + API_KEY);
-    const [data, setData] = useState<object>({});
+
+    interface WheatherData {
+        name?: string;
+        weather?: [{main: string;}]
+        main?: 
+            {
+                temp: number;
+                feels_like: number;
+                humidity: number;
+            }
+        ;
+        wind?: object;
+    }
+    const [data, setData] = useState<WheatherData>({});
     const [city, setCity] = useState<string>("London");
 
     useEffect(() => {
@@ -27,9 +40,24 @@ export function Body() {
         <div className="flex flex-col justify-center items-center gap-2">
             <input type="search" placeholder="Search Places" value={city}
                 onChange={(event) => { setCity(event.target.value) }}
-                className="absolute top-0 right-0 w-7/20 px-4 py-2 border-2 border-gray-400 outline-none focus:shadow-md"
+                className="absolute top-0 right-0 w-1/5 px-4 py-2 border-2 border-gray-400 outline-none focus:shadow-md"
             />
+            <label className="ml-4 mt-24 text-4xl my-4">
+               {data.main?.temp ? Math.round(data.main.temp) : "--"}°
+            </label>
+            <label className="mt-8 text-2xl">
+                {data.name}
+            </label>
+            <label>
+                {data.weather?.[0]?.main}
+            </label>
+            <label>
+                Feels like: {data.main?.feels_like}°C
+            </label>
+            <label>
+                Humidity: {data.main?.humidity}%
+            </label>
+            
         </div>
-
     )
 }
